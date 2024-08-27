48e941c1979ce8102af1e3b1f2d9bf5c4b465dd2
Ryan Hang <ryan.hang3r@gmail.com>
2024-08-26 18:11:55 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# nike_website
+ Nike Website built with NextJS 14 and Tailwind
---
+++ my-project/src/sections/index.js
@@ -0,0 +1,12 @@
+import CustomerReviews from "./CustomerReviews";
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Footer from "./Footer";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+    Hero, CustomerReviews, PopularProducts, Footer, Services, SpecialOffer, Subscribe, SuperQuality
+}
